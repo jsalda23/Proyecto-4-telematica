8c24f1261077d60f155dda245235caa84e2ce6db
jsalda23 <33168449+jsalda23@users.noreply.github.com>
2020-05-30 18:09:39 -0500

    Update ADN_to_RNA-Serial.py
    Versión 1.1: Se modifica para que no requiera especificar carpeta. Ya funciona en el cluster

--- script/ADN_to_RNA-Serial.py
+++ script/ADN_to_RNA-Serial.py
@@ -1,55 +1,49 @@
 import sys
-from os import listdir
 from os.path import isfile, join
+from os import listdir
 from datetime import datetime
+import os
 
 def main():
- 
- start = datetime.now()
- directory = "C:\\Users\\JuanDiego\\Desktop\\Topicos_Telematica\\Proyecto4\\" #sys.argv[1] #1
- 
- fna = [filename for filename in listdir(directory) if filename[-4:] == ".txt"]  #Selecciona los archivos .txt 
-
- for fnafile in fna: #Ciclo para que haga todos los archivos .txt
-  
-  fileName =  directory + fnafile
- 
-  print("File: " + fileName) #Ruta archivos TXT
-
-  f = open(fileName, 'r') #Crea string de texto 
-  
-  res = open('C:\\Users\\JuanDiego\\Desktop\\Topicos_Telematica\\Proyecto4\\opt\\dna\\results\\' + fnafile + '.result', 'w') ##Crea resultado en la carpeta indicada
-  resLine = ""
-  
-  for line in f:
-   #print(line)
-   if line[0] == '>': continue
+    print(os.getcwd())
+    start = datetime.now()
+    fichero = (os.getcwd() + '\\') ##  "C:\\Users\\JuanDiego\\Desktop\\Topicos_Telematica\\Proyecto4\\" #sys.argv[1] #1
+    
+    fna = [archivotexto for archivotexto in listdir(fichero) if archivotexto[-4:] == ".txt"]  #Selecciona los archivos .txt 
+    
+    for fnafile in fna: #Ciclo para que haga todos los archivos .txt
+        fileName =  fichero + fnafile
+        print("File: " + fileName) #Ruta archivos TXT
+        f = open(fileName, 'r') #Crea string de texto 
+        res = open(os.getcwd() + '\\opt\\dna\\results\\' + fnafile + '.result', 'w') ##Crea resultado en la carpeta indicada
+        resLine = ""
+        print(resLine)
+        for line in f:
+            #print(line)
+            if line[0] == '>': continue
    
-   for i in line:
-    #print(i)
-    if i == 't':
-     resLine += 'a'
-    elif i == 'a':
-     resLine += 'u'
-    elif i == 'c':
-     resLine += 'g'
-    elif i == 'g':
-     resLine += 'c'  
-    else:
-     resLine += i
-   resLine += '\n'
-  res.write(resLine)
-
-  f.close()   
-  res.close()
+            for i in line:
+            #print(i)
+                if i == 't':
+                    resLine += 'a'
+                elif i == 'a':
+                    resLine += 'u'
+                elif i == 'c':
+                    resLine += 'g'
+                elif i == 'g':
+                    resLine += 'c'  
+                else:
+                    resLine += i
+                resLine += '\n'
+        res.write(resLine)
+        f.close()   
+        res.close()
   
- end = datetime.now()
- total = end - start
- total_time = total.seconds
- print("Total time: " + str(total_time) + "s")
- 
- 
- #print("Hola")
+    end = datetime.now()
+    total = end - start
+    total_time = total.seconds
+    print("Total time: " + str(total_time) + "s")
+    #print("Hola")
 
 if __name__ == '__main__':
  main()
